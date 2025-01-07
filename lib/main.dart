0706ac8970f6a2dafe0f548bd0fb5654b455631d
Muhammad Ramdhan <evc.adhan021@gmail.com>
2025-01-07 19:05:52 +0700

    sedikit merubah tampilan agar lebih friendly

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
+  runApp(const MaterialApp(
     debugShowCheckedModeBanner: false,
     home: HomePage(),
   ));
@@ -15,32 +15,133 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final List<String> _listItem = [
+    'assets/images/one.jpg',
+    'assets/images/two.jpg',
+    'assets/images/three.jpg',
+    'assets/images/four.jpg',
+    'assets/images/five.jpg',
+    'assets/images/six.jpg',
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey,
+      backgroundColor: Colors.grey[800],
       appBar: AppBar(
         // title: Text('Flutter App'),
         // centerTitle: true,
         backgroundColor: Colors.transparent,
-        leading: Icon(Icons.menu),
+        leading: const Icon(
+          Icons.menu,
+          color: Colors.white,
+        ),
+        title: const Text(
+          "Home",
+          style: TextStyle(color: Colors.white),
+        ),
         actions: [
           Padding(
-            padding: EdgeInsets.all(10),
+            padding: const EdgeInsets.all(10),
             child: Container(
-              width: 30,
+              width: 36,
               height: 30,
               decoration: BoxDecoration(
-                color: Colors.grey[800],
+                color: Colors.grey[900],
                 borderRadius: BorderRadius.circular(10),
               ),
-              child: Center(
-                child: Text('0'),
+              child: const Center(
+                child: Text(
+                  '0',
+                  style: TextStyle(color: Colors.white),
+                ),
               ),
             ),
           )
         ],
       ),
+      body: SafeArea(
+          child: Container(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          children: [
+            Container(
+              height: 250,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                image: const DecorationImage(
+                  image: AssetImage('assets/images/one.jpg'),
+                  fit: BoxFit.cover,
+                ),
+              ),
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  gradient: LinearGradient(
+                    begin: Alignment.bottomRight,
+                    colors: [
+                      Colors.black.withOpacity(.8),
+                      Colors.black.withOpacity(.2),
+                    ],
+                  ),
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    const Text(
+                      'Lifestyle Sale',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 35,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const SizedBox(height: 30),
+                    Container(
+                      height: 50,
+                      margin: const EdgeInsets.symmetric(horizontal: 40),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: Center(
+                        child: Text(
+                          'Shop Now',
+                          style: TextStyle(
+                            color: Colors.grey[900],
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 30),
+                  ],
+                ),
+              ),
+            ),
+            Expanded(
+                child: GridView.count(
+              crossAxisCount: 2,
+              padding: const EdgeInsets.all(20.0),
+              crossAxisSpacing: 20.0,
+              mainAxisSpacing: 20.0,
+              children: [
+                ..._listItem.map((item) => Card(
+                      child: Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          image: DecorationImage(
+                            image: AssetImage(item),
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
+                    )),
+              ],
+            ))
+          ],
+        ),
+      )),
     );
   }
 }
